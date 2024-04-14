61e6772ee40e9df4edfce9911f011104f7e8f675
Stanislas Ormières <stan@stormier.ninja>
2024-04-14 15:00:41 +0200

    feat: ✨ add cors to server.js

--- src/server.js
+++ src/server.js
@@ -1,16 +1,23 @@
 // Import the framework and instantiate it
+import cors from '@fastify/cors'
 import Fastify from 'fastify'
 const fastify = Fastify({
   logger: true
 })
+
+fastify.register(cors)
 
 // Declare a route
 fastify.get('/', async function handler (request, reply) {
   return { hello: 'world' }
 })
 
+fastify.get('/version', async function handler (request, reply) {
+  return reply.send('1.0.0')
+})
+
 // Get port from environment
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
 // Run the server!
 try {
